328e1a7a680ea1c123ba5130852c77bb091b2a6b
민경훈 <148615488+oneplast@users.noreply.github.com>
2026-09-14 11:45:19 +0900

    Fix typo in link text for GitHub Action CI/CD

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction CI/CD 4!
+          Learn GithubAction CI/CD 4!!
         </a>
       </header>
     </div>
